e998bc1efd249b13387a0dbf220eb2d427859464
martincorona007 <martincorona007@hotmail.com>
2020-02-19 12:51:27 -0600

    removing the option to ask user number

--- Program 2 Matrix Circular/main.cpp
+++ Program 2 Matrix Circular/main.cpp
@@ -6,7 +6,7 @@
 int matrix_1[20][20];
 float matrix_2[20][20];
 
-int n,m,number,aux_i,aux_j,aux_i1,aux_j1,aux_i2,aux_j2,sum=0,sum1=0,size,rule_1=0,op;
+int n,m,number,aux_i,aux_j,aux_i1,aux_j1,aux_i2,aux_j2,sum=0,sum1=0,size,rule_1=0,op,d1,d2;
 int aux_i3,aux_j3,aux_i4,aux_j4;//for coner left side
 int sect_a=0,sect_b=0,sect_c=0,sect_d=0,sect_e=0,sect_f=0,sect_g=0,sect_h=0,sect_i=0;
 bool flag_1=true,flag_2,flag_3=true;
@@ -46,16 +46,16 @@ int main()
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 printf("Write for [%i,%i]= ",i,j);
-                scanf("%i",&data);
+                //scanf("%i",&data);
                 //data=rand()%50+1;
-                matrix_1[i][j]=data;
+                matrix_1[i][j]=1;
             }
         }
         //fill in the other matrix
         for (int i = 0; i < n ;i++) {
             for (int j=0; j < m; j++)
             {
-            matrix_2[i][j]=0;
+                matrix_2[i][j]=0;
             }         
         }
         
@@ -73,34 +73,12 @@ int main()
                     printf(" [%i]{%i,%i} ",matrix_2[i][j],i,j);
                 }
             }
-                
-        do{
-          //s  printf("\nPOINT4 [%i,%i] + [%i,%i]",aux_i1,aux_j1,aux_i2,aux_j2);
-                  
-            printf("\n\nSelect number: ");
-            scanf("%i",&number);
-            
             for(int i=0;i<n;i++){//FIND THE NUMBER AND VERIFY IF EXITS
                 for(int j=0;j<m;j++){
-                    if(flag_3){
-                        if(matrix_1[i][j]==number){
                             printf("Yes [%i,%i]",i,j);
-                            aux_i=i;
-                            aux_j=j;
-                            flag_2=true;
-                            
-                            flag_3=false;
-                        }else{
-                            flag_2=false;
-                        }
+                            d1=aux_i=i;
+                            d2=aux_j=j;
 
-                    }
-                }
-            }
-                        //VERIFY IF THE NUMBER EXIST
-            if(flag_2==false){
-                printf("Number not found");
-            }else if(flag_2==true){
                 printf("out [%i,%i]",aux_i,aux_j);
                 
                 aux_i3=aux_i4=aux_i;
@@ -119,6 +97,7 @@ int main()
                 rule_1=n;
                 rule_1-=1;
                 printf("\n aux_i2 %i rule_1 %i ",aux_i2,rule_1);
+                
                 //SELECT THE SIDE
                 if(aux_i2>rule_1){//BELOW SIDE
                     sect_a=1;
@@ -366,7 +345,6 @@ int main()
                     matrix_2[aux_i][aux_j]=sum1/9;
 
                 }
-            //    printf("\nPOINT5 [%i,%i] + [%i,%i]",aux_i1,aux_j1,aux_i2,aux_j2);
                 //CENTER SIDES
                 if(sect_b){
                     for(int x=aux_i1;x<=aux_i2;x++){
@@ -377,7 +355,28 @@ int main()
                     }
                     matrix_2[aux_i][aux_j]=sum1/9;
 	            }
+                if(sect_a==1||sect_b==1||sect_c==1||sect_d==1||sect_e==1||sect_f==1||sect_g==1||sect_h==1||sect_i==1){//CLEAN DATA
+                aux_j=0;
+                aux_i=0;
+               printf("\nPOINT20 [%i,%i] + [%i,%i] %i %i ",aux_i1,aux_j1,aux_i2,aux_j2,aux_i,aux_j);
                 
+               printf("INSIDE");
+                sum1=0;
+                sect_b=0;
+                sect_a=0;
+                sect_c=0;
+                sect_d=0;
+                sect_e=0;
+                sect_f=0;
+                sect_g=0;
+                sect_h=0;
+                sect_i=0;
+            }
+
+
+
+                }
+            }
                 printf("\nMatrix 1x");
                 for(int i=0;i<n;i++){
                         printf("\n");
@@ -391,30 +390,8 @@ int main()
                     for(int j=0;j<m;j++){
                         printf(" [%i]{%i,%i} ",matrix_2[i][j],i,j);
                     }
-                }
-                printf("\nthe sum %i",sum1);
-            
-            }
-
-            printf("\n\nSelect 1 to finish 0 to cotinue: ");
-            scanf("%i",&op);
-            if(op==0){//CLEAN DATA
-                flag_3=true;
-                aux_j=0;
-                aux_i=0;
-                sum1=0;
-                sect_b=0;
-                sect_a=0;
-                sect_c=0;
-                sect_d=0;
-                sect_e=0;
-                sect_f=0;
-                sect_g=0;
-                sect_h=0;
-                sect_i=0;
-            }
-
-        }while (op!=1);
+                }       
+       
     }
     return 0;
 }
