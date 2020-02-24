a2796953143475036e8d6ec98aa6f340977a117a
martincorona007 <martincorona007@hotmail.com>
2020-02-24 11:17:10 -0600

    adding comments

--- Program 1 Matrix N by N/main.cpp
+++ Program 1 Matrix N by N/main.cpp
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <ctime>
 
-int matrix_1[20][20];
-int matrix_2[20][20];
-int result[20][20];
-int n1,m1,n2,m2,res,n3,m3;
-int flag_a=0;
+int matrix_1[20][20];//Matrix_1 is going to be the first matrix 
+int matrix_2[20][20];//Matrix_2 is going to be the second matrix
+int result[20][20];//Result is going to be the output of the operation
+int n1,m1,n2,m2,res,n3,m3;//Matrix's Sizes
+int flag_a=0;//Check if the condition was true
+
 int main(){
-    srand((unsigned)NULL);
+   //Ask data
     printf("Matrix 1");
     printf("\nSize R: ");
         scanf("%i",&n1);
@@ -20,69 +21,64 @@ int main(){
         scanf("%i",&n2);
     printf("Size C: ");
         scanf("%i",&m2);
-    
-    if(m1==n2){//VALIDATE THE MATRIX
+    //Validate the matrix
+    if(m1==n2){
         flag_a=1;
     }else{
         printf("Error Matrix");
     }
 
     if(flag_a==1){
-            //create size of the output matrix 
+        //Create matrix's output size 
             n3=n1;
             m3=m2;
             result[n3][m3];
-            //FILL THE MATRIX 1
+        //Fill the matrix 1
         int data;
         for(int i=0;i<n1;i++){
-        for(int j=0;j<m1;j++){
-                    //printf("Write for [%i,%i]= ",i,j);
-                //  scanf("%i",&data);
-                    data=rand()%50+1;
-                    matrix_1[i][j]=data;
+            for(int j=0;j<m1;j++){
+               printf("Write for [%i,%i]= ",i,j);
+                scanf("%i",&data);
+               matrix_1[i][j]=data;
+            }
         }
-        }
-        //FILL THE MATRIX 2
+        //Fill the matrix 2
         int data1;
         for(int i=0;i<n2;i++){
-        for(int j=0;j<m2;j++){
-                    //printf("Write for [%i,%i]= ",i,j);
-                //  scanf("%i",&data);
-                    data1=rand()%50+1;
-                    matrix_2[i][j]=data1;
+            for(int j=0;j<m2;j++){
+               printf("Write for [%i,%i]= ",i,j);
+                scanf("%i",&data1);
+               matrix_2[i][j]=data1;
+            }
         }
-        }
-        //OPERATION
-        for (int i = 0; i < n1; i++)
-        {
-            for (int j = 0; j < m2; j++)
-            {
-                for (int x = 0; x < m1; x++)
-                {
+        //Operation
+        for (int i = 0; i < n1; i++){
+            for (int j = 0; j < m2; j++){
+                for (int x = 0; x < m1; x++){
                     result[i][j]=matrix_1[i][x]*matrix_2[x][j]+result[i][j];
                 }
             }
         }
-        //print
+        //Print the Matrix
         printf("\nMatrix 1");
         for(int i=0;i<n1;i++){
-                printf("\n");
+            printf("\n");
             for(int j=0;j<m1;j++){
-                            printf(" [%i]{%i,%i} ",matrix_1[i][j],i,j);
+                printf(" [%i]{%i,%i} ",matrix_1[i][j],i,j);
             }
         }
         printf("\nMatrix 2");
         for(int i=0;i<n2;i++){
-                printf("\n");
+            printf("\n");
             for(int j=0;j<m2;j++){
-                            printf(" [%i]{%i,%i} ",matrix_2[i][j],i,j);
+                 printf(" [%i]{%i,%i} ",matrix_2[i][j],i,j);
             }
         }
         printf("\nOutPut ");
         for(int i=0;i<n3;i++){
-                printf("\n");
+            printf("\n");
             for(int j=0;j<m3;j++){
-                            printf(" [%i]{%i,%i} ",result[i][j],i,j);
+                printf(" [%i]{%i,%i} ",result[i][j],i,j);
             }
         }
     }
