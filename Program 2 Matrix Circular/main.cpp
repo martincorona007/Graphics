bdc2d1d322c50820eb59d4c5ae48dbe10053259e
martincorona007 <martincorona007@hotmail.com>
2020-02-26 07:08:53 -0600

    adding comments

--- Program 2 Matrix Circular/main.cpp
+++ Program 2 Matrix Circular/main.cpp
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include <ctime>
 
+int matrix_1[20][20];//Matrix_1 is going to be the first matrix 
+float matrix_2[20][20];//Matrix_2 is going to be the second matrix
+/*
+n and m are the matrix's size
+aux_i,aux_j,aux_i1,aux_j1,aux_i2,aux_j2 are the variables to take position in the array
+sum1 used to sum all the value of the matrix
+rule_1 get the position of the array
+*/
+int n,m,aux_i,aux_j,aux_i1,aux_j1,aux_i2,aux_j2,sum1=0,rule_1=0;
+int aux_i3,aux_j3,aux_i4,aux_j4;//Varibles for coner left and right side
+int sect_a=0,sect_b=0,sect_c=0,sect_d=0,sect_e=0,sect_f=0,sect_g=0,sect_h=0,sect_i=0;//Selection of the side
+bool flag_1=true;//Validate the matrix is greater than three
 
-int matrix_1[20][20];
-float matrix_2[20][20];
-
-int n,m,number,aux_i,aux_j,aux_i1,aux_j1,aux_i2,aux_j2,sum=0,sum1=0,size,rule_1=0,op,d1,d2;
-int aux_i3,aux_j3,aux_i4,aux_j4;//for coner left side
-int sect_a=0,sect_b=0,sect_c=0,sect_d=0,sect_e=0,sect_f=0,sect_g=0,sect_h=0,sect_i=0;
-bool flag_1=true,flag_2,flag_3=true;
-
-void print();
 
 int main()
 {
-    srand((unsigned)NULL);
-
+    //Ask Matrix's size
     printf("Write the Matrix Size \n");
 
     printf("Size N ");
@@ -25,7 +27,7 @@ int main()
     printf("Size M ");
         scanf("%i",&m);
         
-    while(n<3&&m<3){//make the user insert N and M grater than 3
+    while(n<3&&m<3){//Make the user insert N and M grater than 3
         printf("Error Matrix Size");
         flag_1=false;
         printf("\nSize N ");
@@ -35,30 +37,30 @@ int main()
             scanf("%i",&m);
          
     }
-    if(n>=3&&m>=3){//change the flag 
-            flag_1=true;
+    if(n>=3&&m>=3){//Validate the matrix is greater than three
+            flag_1=true;//change the flag 
     }
-    if(flag_1==true){
-        int matrix_1[n][m];
-        int matrix_2[n][m];
+    if(flag_1==true){//Check if the above condition was true
+        int matrix_1[n][m];//Create matrix's size
+        int matrix_2[n][m];//Create matrix's size
         int data;
-        //ask the data
+        //Ask the data
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 printf("Write for [%i,%i]= ",i,j);
-                //scanf("%i",&data);
+                scanf("%i",&data);
                 //data=rand()%50+1;
-                matrix_1[i][j]=1;
+                matrix_1[i][j]=data;
             }
         }
-        //fill in the other matrix
+        //Fill in the other matrix
         for (int i = 0; i < n ;i++) {
             for (int j=0; j < m; j++)
             {
                 matrix_2[i][j]=0;
             }         
         }
-        
+        //Print both Matrix
         printf("\nMatrix 1");
             for(int i=0;i<n;i++){
                     printf("\n");
@@ -73,66 +75,73 @@ int main()
                     printf(" [%i]{%i,%i} ",matrix_2[i][j],i,j);
                 }
             }
-            for(int i=0;i<n;i++){//FIND THE NUMBER AND VERIFY IF EXITS
-                for(int j=0;j<m;j++){
-                            printf("Yes [%i,%i]",i,j);
-                            d1=aux_i=i;
-                            d2=aux_j=j;
+        
+        for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+                           
+                aux_i=i;
+                aux_j=j;
 
-                printf("out [%i,%i]",aux_i,aux_j);
-                
                 aux_i3=aux_i4=aux_i;
                 aux_j3=aux_j4=aux_j;
                 
                 aux_i3-=1;aux_j3+=1;
                 aux_i4+=1;aux_j4-=1;
+/*
 
+[      ,      ][      ,      ][      ,      ][aux_i3,aux_j3][      ,      ]
+[      ,      ][      ,      ][ aux_i,aux_j ][      ,      ][      ,      ]
+[      ,      ][aux_i4,aux_j4][      ,      ][      ,      ][      ,      ]
+*/
                 aux_i1=aux_i2=aux_i;
                 aux_j1=aux_j2=aux_j;
-
+/*
+[      ,      ][aux_i1,aux_j1][      ,      ][      ,      ][      ,      ]
+[      ,      ][      ,      ][ aux_i,aux_j ][      ,      ][      ,      ]
+[      ,      ][      ,      ][      ,      ][aux_i2,aux_j2][      ,      ]
+*/
                 aux_i1-=1;aux_j1-=1;
                 aux_i2+=1;aux_j2+=1;
                 printf("\nPOINT1 [%i,%i] + [%i,%i]",aux_i1,aux_j1,aux_i2,aux_j2);
                 //RULES TO KNOW IF 
                 rule_1=n;
                 rule_1-=1;
-                printf("\n aux_i2 %i rule_1 %i ",aux_i2,rule_1);
                 
                 //SELECT THE SIDE
-                if(aux_i2>rule_1){//BELOW SIDE
+              if(aux_i2>rule_1){//BELOW SIDE
                     sect_a=1;
-                }
-                if(aux_i2<=rule_1){//CENTER SIDES
+              }
+              if(aux_i2<=rule_1){//CENTER SIDES
                     sect_b=1;
-                }
-                if(aux_i1<0){//ABOVE SIDE
+              }
+              if(aux_i1<0){//ABOVE SIDE
                     sect_c=1;
                     sect_b=0;
                     sect_a=0;
-                }
-                if(aux_j2>rule_1){//RIGHT SIDE
+              }
+              if(aux_j2>rule_1){//RIGHT SIDE
                     sect_d=1;
                     sect_b=0;
-                }
-                if(aux_j1<0){//LEFT SIDE
+              }
+              if(aux_j1<0){//LEFT SIDE
                     sect_e=1;
                     sect_b=0;
-                }
-                if(aux_i2>rule_1&&aux_j2>rule_1){//BELOW RIGHT SIDE CONER 
+              }
+              if(aux_i2>rule_1&&aux_j2>rule_1){//BELOW RIGHT SIDEprogramas de escuela con graficos CONER 
                     sect_f=1;
                     sect_b=0;
                     sect_d=0;
                     sect_a=0;
-                }
-                if(aux_i4>rule_1&&aux_j4<0){//BELOW LEFT SIDE CONER 
+              }
+              if(aux_i4>rule_1&&aux_j4<0){//BELOW LEFT SIDE CONER 
                     sect_g=1;
                     sect_f=0;
                     sect_b=0;
                     sect_d=0;
                     sect_a=0;
                     sect_e=0;
-                }
-                if(aux_i1<0&& aux_j1<0){//ABOVE LEFT SIDE CONER
+              }
+              if(aux_i1<0&& aux_j1<0){//ABOVE LEFT SIDE CONER
                     sect_h=1;
                     sect_g=0;
                     sect_f=0;
@@ -141,8 +150,8 @@ int main()
                     sect_a=0;
                     sect_e=0;
                     sect_c=0;
-                }
-                if(aux_i3<0&& aux_j3>rule_1){//ABOVE RIGHT SIDE CONER
+              }
+              if(aux_i3<0&& aux_j3>rule_1){//ABOVE RIGHT SIDE CONER
                     sect_i=1;
                     sect_h=0;
                     sect_g=0;
@@ -152,9 +161,9 @@ int main()
                     sect_a=0;
                     sect_e=0;
                     sect_c=0;
-                }
+              }
                 //ABOVE RIGHT SIDE CONER
-                if(sect_i){
+              if(sect_i){
                     aux_i3+=1;
                     aux_j3-=2;
                     aux_j4+=1;
@@ -181,9 +190,9 @@ int main()
                     
                     sum1+=matrix_1[rule_1][0];
                     matrix_2[aux_i][aux_j]=sum1/9;
-                }
+              }
                 //ABOVE LEFT SIDE CONER
-                if(sect_h){
+              if(sect_h){
                     aux_i1+=1;
                     aux_j1+=1;
                     for(int x=aux_i1;x<=aux_i2;x++){
@@ -209,7 +218,7 @@ int main()
                     
                     sum1+=matrix_1[rule_1][rule_1];
                     matrix_2[aux_i][aux_j]=sum1/9;
-                }
+               } 
                 //BELOW LEFT SIDE CONER 
                 if(sect_g){
                     aux_j3-=1;
@@ -355,28 +364,23 @@ int main()
                     }
                     matrix_2[aux_i][aux_j]=sum1/9;
 	            }
-                if(sect_a==1||sect_b==1||sect_c==1||sect_d==1||sect_e==1||sect_f==1||sect_g==1||sect_h==1||sect_i==1){//CLEAN DATA
-                aux_j=0;
-                aux_i=0;
-               printf("\nPOINT20 [%i,%i] + [%i,%i] %i %i ",aux_i1,aux_j1,aux_i2,aux_j2,aux_i,aux_j);
-                
-               printf("INSIDE");
-                sum1=0;
-                sect_b=0;
-                sect_a=0;
-                sect_c=0;
-                sect_d=0;
-                sect_e=0;
-                sect_f=0;
-                sect_g=0;
-                sect_h=0;
-                sect_i=0;
-            }
-
-
-
+                //CLEAN DATA
+                if(sect_a==1||sect_b==1||sect_c==1||sect_d==1||sect_e==1||sect_f==1||sect_g==1||sect_h==1||sect_i==1){
+                    aux_j=0;
+                    aux_i=0;
+                    sum1=0;
+                    sect_b=0;
+                    sect_a=0;
+                    sect_c=0;
+                    sect_d=0;
+                    sect_e=0;
+                    sect_f=0;
+                    sect_g=0;
+                    sect_h=0;
+                    sect_i=0;
                 }
-            }
+             }
+        }
                 printf("\nMatrix 1x");
                 for(int i=0;i<n;i++){
                         printf("\n");
