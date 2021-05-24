64a6213f76528a5571250808911ecd45e3546860
Don <martincorona007@hotmail.com>
2021-05-24 17:57:44 -0500

    cleaning code

--- Processing Images/main.py
+++ Processing Images/main.py
@@ -270,8 +270,7 @@ def save_pixelH(R,G,B,x,a,b):
             matrix_savePixel[x][y] = G
         if control == 3:
             matrix_savePixel[x][y] = B
-        control = control + 1 
-    
+        control = control + 1  
 def save_pixelsbA(R,G,B,doc):
     control = 1
     for x in range(0,3):
@@ -340,6 +339,7 @@ def save_pixelsbA(R,G,B,doc):
                 if control == 27:
                     matrix_savePixel[x][y] = B
             control = control + 1 
+
 #>>>>>>>>>>>>>>>>>>>>>>>>OPERARTION<<<<<<<<<<<<<<<<<<<<<<<<<
 def operation():
     listnewpixel = []
@@ -347,59 +347,59 @@ def operation():
     for li in range(3):
         for lo in range(9):
             if sector == 1:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][0])
             if sector == 2:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][0])
             if sector == 3:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][0])
             if sector == 4:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][1])
             if sector == 5:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][1])
             if sector == 6:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][1])
             if sector == 7:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][2])
             if sector == 8:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][2])
             if sector == 9:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[0][2])
             if sector == 10:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][0])
             if sector == 11:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][0])
             if sector == 12:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][0])
             if sector == 13:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][1])
             if sector == 14:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][1])
             if sector == 15:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][1])
             if sector == 16:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][2])
             if sector == 17:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][2])
             if sector == 18:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[1][2])
             if sector == 19:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][0])
             if sector == 20:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][0])
             if sector == 21:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][0])
             if sector == 22:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][1])
             if sector == 23:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][1])
             if sector == 24:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][1])
             if sector == 25:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][2])
             if sector == 26:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][2])
             if sector == 27:
-                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]* matrix_convolution[2][2])
             
             sector = sector + 1
 
@@ -408,7 +408,7 @@ def operation():
     #remove negative numbers
     for clx in range(3):
         for cly in range(9):
-            if matrix_saveOperation[clx][cly]<0:
+            if matrix_saveOperation[clx][cly] < 0:
                 matrix_saveOperation[clx][cly] = matrix_saveOperation[clx][cly]*-1
     print_so()
     R = 0
@@ -418,6 +418,7 @@ def operation():
         R += matrix_saveOperation[oxl][0] + matrix_saveOperation[oxl][3] + matrix_saveOperation[oxl][6]
         G += matrix_saveOperation[oxl][1] + matrix_saveOperation[oxl][4] + matrix_saveOperation[oxl][7]
         B += matrix_saveOperation[oxl][2] + matrix_saveOperation[oxl][5] + matrix_saveOperation[oxl][8]
+    
     print("1p R "+str(R)+" G "+str(G)+" B "+str(B))
     if bias == 1:
         print("with bias")
@@ -450,54 +451,54 @@ def operation():
 
 print("Matrix 3 x 3")
 
-n=3
+n = 3
 matrix_convolution = [[0] * n for i in range(n)]
 matrix_pre = [[0] * n for i in range(n)]#save fraction
 #FUNCTIONS
 #Fill the matrix
-anw=int(input("Any average? yes=1 no=0 "))
+anw = int(input("Any average? yes=1 no=0 "))
 if anw == 1:
-    numerator=int(input("Numerator: "))
-    denominator=int(input("Denominator: "))
-    frac=numerator/denominator
-    lc=round(frac,2)#get the precision after the decimal point.
+    numerator = int(input("Numerator: "))
+    denominator = int(input("Denominator: "))
+    frac = numerator / denominator
+    lc = round(frac,2)#get the precision after the decimal point.
 else:
-    lc=1
-anws=int(input("Do you want bias? yes=1 no=0 "))
-if anws==1:
-    bias=1
-    anwsr=int(input("Number bias: "))
-    biasv=anwsr
+    lc = 1
+anws = int(input("Do you want bias? yes=1 no=0 "))
+if anws == 1:
+    bias = 1
+    anwsr = int(input("Number bias: "))
+    biasv = anwsr
 else:
-    bias=0
+    bias = 0
 """
 The [matrix_pre] is to get a float number and multiple it by the original convolution mask[matrix_convolution].
 """
 for i in range(n):
     for j in range(n):
-        matrix_pre[i][j]=lc
+        matrix_pre[i][j] = lc
         
 #print_mk()
 print("Convolution Mask")
 for i in range(n):
     for j in range(n):
-        matrix_convolution[i][j]=int(input("Enter number: "))
+        matrix_convolution[i][j] = int(input("Enter number: "))
         print_mc()
 for i in range(n):
     for j in range(n):
-        matrix_convolution[i][j]=matrix_pre[i][j]*(matrix_convolution[i][j])
+        matrix_convolution[i][j] = matrix_pre[i][j] * (matrix_convolution[i][j])
 
 print_mc()
 
-n=3
-matrix_1=[[0]* n for i in range(n)]#Get the Source pixel 
+n = 3
+matrix_1 = [[0]* n for i in range(n)]#Get the Source pixel 
 
 #matrix_2=[[0]* n for i in range(n)]
 #width, height= img.size
-width=img.shape[1]
-height=img.shape[0]
+width = img.shape[1]
+height = img.shape[0]
 
-outputImg=np.zeros([height,width,3],dtype=np.uint8)
+outputImg = np.zeros([height,width,3],dtype=np.uint8)
 
 for itx in range(height):
     for ity in range(width):
--- Processing Images/testmatrix.py
+++ Processing Images/testmatrix.py
@@ -1,7 +1,7 @@
-ns=9
-ns1=3
-matrix_savePixel= [[0] * ns for i in range(ns1)]
-matrix_saveOperation=[[0] * ns for i in range(ns1)]
+ns = 9
+ns1 = 3
+matrix_savePixel =  [[0] * ns for i in range(ns1)]
+matrix_saveOperation = [[0] * ns for i in range(ns1)]
 def print_mc():
     print("matrix savepixel")
     for row in matrix_savePixel:
@@ -10,15 +10,75 @@ def print_mc():
 
 for x in range(0,2):
     for y in range(0,6):
-        matrix_savePixel[x][y]=1
+        matrix_savePixel[x][y] = 1
 print_mc()
 for x in range(0,2):
     for y in range(6,9):
-        matrix_savePixel[x][y]=2
+        matrix_savePixel[x][y] = 2
 print_mc()
 for y in range(6):
-    matrix_savePixel[2][y]=3
+    matrix_savePixel[2][y] = 3
 print_mc()
 for y in range(6,9):
-    matrix_savePixel[2][y]=4
+    matrix_savePixel[2][y] = 4
 print_mc()
+
+def operation():
+    listnewpixel = []
+    sector = 1
+    for li in range(3):
+        for lo in range(9):
+            if sector == 1:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+            if sector == 2:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+            if sector == 3:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][0])
+            if sector == 4:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+            if sector == 5:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+            if sector == 6:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][1])
+            if sector == 7:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+            if sector == 8:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+            if sector == 9:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[0][2])
+            if sector == 10:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+            if sector == 11:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+            if sector == 12:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][0])
+            if sector == 13:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+            if sector == 14:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+            if sector == 15:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][1])
+            if sector == 16:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+            if sector == 17:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+            if sector == 18:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[1][2])
+            if sector == 19:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+            if sector == 20:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+            if sector == 21:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][0])
+            if sector == 22:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+            if sector == 23:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+            if sector == 24:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][1])
+            if sector == 25:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
+            if sector == 26:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
+            if sector == 27:
+                matrix_saveOperation[li][lo] = int(matrix_savePixel[li][lo]*matrix_convolution[2][2])
